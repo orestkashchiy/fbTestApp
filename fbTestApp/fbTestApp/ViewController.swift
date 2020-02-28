2d35980b70dde7496fa4e2d74ef02ac2325b6f2e
Orest Kashchiy <okashchiy@gmail.com>
2020-02-28 13:12:14 +0300

    create project

---
+++ fbTestApp/fbTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fbTestApp
+//
+//  Created by Admin on 28.02.2020.
+//  Copyright © 2020 com.orestkashchiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
